cbe8d8cd13b3441a2a4776b4e72e9c353b15cbc8
Robert Barrero <barrero.zizer.95@gmail.com>
2020-05-17 22:55:06 +0200

    Test_PaymentData_v1, Test_User_v1

---
+++ Test_User.py
@@ -0,0 +1,15 @@
+from . import User
+
+
+def test_user_ctor():
+    # Test de la inicialització
+
+    user = User('Jose Mari', '98765432J', 'C. MatalasCañas, 78', 'joselito@pepemail.com', '999666999')
+
+    assert user.nomComplet == 'Jose Mari'
+    assert user.DNI == '98765432J'
+    assert user.direcio == 'C. MatalasCañas, 78'
+    assert user.email == 'joselito@pepemail.com'
+    assert user.telefon == '999666999'
+
+
