f628508b2c7166a91c808afb880f9e93c64629e0
Robert Barrero <barrero.zizer.95@gmail.com>
2020-05-21 13:04:25 +0200

    Cars class v1

--- Cars.py
+++ Cars.py
@@ -1,4 +1,11 @@
 class Cars:
 
-    def __init__(self):
+    def __init__(self,idReserva, matricula, marca, diesReservat):
+
+        self.idReserva = idReserva
+        self.matricula = matricula
+        self.marca = marca
+        self.diesReservat = diesReservat
+
         pass
+
