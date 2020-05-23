c7e3ffc7b1dedf72d9724bcca1c5835166910f24
Robert Barrero <barrero.zizer.95@gmail.com>
2020-05-23 10:34:52 +0200

    prueba test reintentar reserva

--- Tests_versio_4/test_Reintentar_ReservaVol.py
+++ Tests_versio_4/test_Reintentar_ReservaVol.py
@@ -1,44 +1,45 @@
-from unittest import mock
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
 from User import User
 from Viatge import Viatge
-from unittest import TestCase
-from Skyscanner import Skyscanner
+from unittest import mock
 
-u1 = User("George Hill Rogers", "47117447J", "Brooklyn", "rubiioo00_fcb_@hotmail.com", "983")
-tipus_targeta = ["VISA", "PagosPaco", "MasterCard"]
-num_targeta = [12345, 54321, 2456342]
-codi_seguretat = [78365, 53420987, 23452325]
 
-v1 = Viatge(u1, 2, ["Paris"])
-v2 = Viatge(u1, 3, ["New York", "Buenos Aires"])
-v3 = Viatge(u1, 2, ["Paris", "Roma", "Tokio, Buenos Aires"])
-v4 = Viatge(u1, 6, ["Paris", "Roma","New York", "Buenos Aires", "Tokio"])
-v5 = Viatge(u1, 4, ["Paris", "Roma", "Tokio, Buenos Aires"])
+steverogers=User("Steve Grant Rogers", "471","Brooklyn","sgr@gmail.com","983")
+tipus_targeta=["VISA","VISA","MasterCard"]
+num_targeta=[12345,54321,2456342]
+codi_seguretat=[78365,53420987,23452325]
 
-v1.pagar(tipus_targeta[1], num_targeta[2], codi_seguretat[0])
-v2.pagar(tipus_targeta[2], num_targeta[2], codi_seguretat[0])
-v4.pagar(tipus_targeta[0], num_targeta[2], codi_seguretat[0])
-v5.pagar(tipus_targeta[0], num_targeta[2], codi_seguretat[0])
+v1=Viatge(steverogers,4,["Paris", "Roma","New York", "Buenos Aires", "Rio de Janeiro"])
+v2=Viatge(steverogers,10,["Moscu","Atenas","Milan","Londres"])
+v3=Viatge(steverogers,2,["Tokyo"])
+v4=Viatge(steverogers,4,["Paris", "Roma","New York", "Buenos Aires"])
+
+v1.pagar("VISA",24352,22452)
+v2.pagar("MasterCard",234254,789080)
+v4.pagar("VISA", 975674,523467)
 
 class test_Reintentar_PagamentViatge(unittest.TestCase):
 
-    @mock.patch('Skyscanner.Skyscanner.confirm_reserve')
+    #@mock.patch('Skyscanner.Skyscanner.confirm_reserve')
 
-    def test_Reintentar_PagamentViatge(self, mock_skyscanner):
-
+    def test_Reintentar_PagamentViatge(self):
+        """
         assert v1.confirma_reserva() == False
         assert v2.confirma_reserva() == True
         assert v3.confirma_reserva() == False
         assert v4.confirma_reserva() == False
         assert v5.confirma_reserva() == True
 
-        # A continuacio es prova amb skyscanner retornant false amb la funció confirm_reserve
-
         mock_skyscanner.return_value = False
 
         assert v2.confirma_reserva() == False
         assert v5.confirma_reserva() == False
+        """
 
-
-
+        assert v1.confirma_reserva()==False
+        assert v2.confirma_reserva()==True
+        assert v3.confirma_reserva()==False
+        assert v4.confirma_reserva()==True
 
